5418eab89beeb9cccfbc12e0a73a1303ac9c6b05
cristaldevil <cristaldevil@mail.ru>
2025-01-28 16:38:00 +0700

    Удалены встроеные файлы миграций. Добавлены миграции ролей и пользователей.

---
+++ database/migrations/2025_01_28_090723_create_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('surname', 64);
+            $table->string('name', 64);
+            $table->string('patronymic', 64)->nullable();
+            $table->boolean('sex');
+            $table->date('birthday');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('api_token', 60)->nullable()->unique();
+            $table->foreignId('role_id')->constrained('roles', 'id')->cascadeOnUpdate();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
